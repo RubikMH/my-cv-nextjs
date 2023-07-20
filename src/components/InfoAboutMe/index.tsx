8d7077513174f6ab1286a94868d1771f1c44c4ac
RubikMH <mohamadhasans152@gmail.com>
2023-07-20 20:49:58 +0330

    some change

--- src/components/InfoAboutMe/index.tsx
+++ src/components/InfoAboutMe/index.tsx
@@ -1,12 +1,12 @@
 import React from 'react'
 
-const InfoAboutMe = () => (
+const InfoAboutMe: React.FC = () => (
   <div className="max-w-6xl mx-auto mt-20 bg-layout rounded-md text-primary flex ">
     <div className="bg-[url(/mee.jpg)] h-[30rem] w-1/2 bg-cover rounded-md">
       <div className="bg-gray-800 w-full  bg-opacity-30 h-full rounded-md" />
     </div>
     <div className=" px-6 py-4 h-[30rem] w-1/2 bg-cover rounded-md">
-      <div>
+      <div className="text-justify ">
         <h3>RubikMH</h3>
         <code className="text-justify">
           Lorem ipsum, dolor sit amet consectetur adipisicing elit. Non
