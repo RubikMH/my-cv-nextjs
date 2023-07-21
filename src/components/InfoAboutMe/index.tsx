79f799e3565f3799076f6c89c7e459db14d1518c
RubikMH <mohamadhasans152@gmail.com>
2023-07-21 11:17:26 +0330

    some change and add icons change size logo and photo

--- src/components/InfoAboutMe/index.tsx
+++ src/components/InfoAboutMe/index.tsx
@@ -1,11 +1,14 @@
 import React from 'react'
+import { GoDotFill, GoDot } from 'react-icons/go'
+import { AiFillLinkedin, AiFillTwitterSquare } from 'react-icons/ai'
+import { FaGithubSquare } from 'react-icons/fa'
 
 const InfoAboutMe: React.FC = () => (
   <div className="max-w-6xl mx-auto mt-20 bg-layout rounded-md text-primary flex ">
-    <div className="bg-[url(/mee.jpg)] h-[30rem] w-1/2 bg-cover rounded-md">
+    <div className="bg-[url(/myself.jpg)] h-[30rem] w-1/3 bg-contain bg-no-repeat bg-center rounded-md">
       <div className="bg-gray-800 w-full  bg-opacity-30 h-full rounded-md" />
     </div>
-    <div className=" px-6 py-4 h-[30rem] w-1/2 bg-cover rounded-md">
+    <div className=" px-6 py-4 h-[30rem] w-3/4 relative bg-cover rounded-md">
       <div className="text-justify ">
         <h3>RubikMH</h3>
         <code className="text-justify">
@@ -17,6 +20,25 @@ const InfoAboutMe: React.FC = () => (
           corrupti incidunt minima tempora optio voluptatum magni quod? Fuga
           ratione magnam repellat quibusdam distinctio!
         </code>
+        <div className="flex absolute top-5 right-5">
+          <GoDotFill size={25} className="cursor-pointer" />
+          <GoDot size={25} className="cursor-pointer" />
+          <GoDot size={25} className="cursor-pointer" />
+        </div>
+        <div className="absolute bottom-10 right-10 flex gap-4">
+          <FaGithubSquare
+            size={35}
+            className="cursor-pointer text-primary hover:text-[#000000] transition-all duration-300"
+          />
+          <AiFillLinkedin
+            size={35}
+            className="cursor-pointer text-primary hover:text-[#0A66C2] transition-all duration-300"
+          />
+          <AiFillTwitterSquare
+            size={35}
+            className="cursor-pointer text-primary hover:text-[#1D9BF0] transition-all duration-300"
+          />
+        </div>
       </div>
     </div>
   </div>
--- src/containers/Layout/Header/index.tsx
+++ src/containers/Layout/Header/index.tsx
@@ -1,9 +1,14 @@
 import React from 'react'
+import { FiActivity } from 'react-icons/fi'
 
 const Header = () => (
   <header className="h-28 flex flex-col justify-between items-center border-b border-solid mb-6 border-primary">
-    <h2 className=" mb-2 text-xl">
-      RUBIK<span className="text-layout text-2xl">-/-</span>MH
+    <h2 className=" mb-2 mt-7 ">
+      RUBIK
+      <span className="text-layout ">
+        <FiActivity className="inline-block" />
+      </span>
+      MH
     </h2>
     <nav className="w-full">
       <ul className="w-full flex flex-wrap justify-evenly">
