3b4dbfe476059597ec63d04eea9a13722c2014d8
RubikMH <mohamadhasans152@gmail.com>
2023-07-29 19:43:00 +0330

    add swiper

--- src/components/Skills/index.tsx
+++ src/components/Skills/index.tsx
@@ -1,27 +1,153 @@
+/* eslint-disable object-curly-newline */
 /* eslint-disable import/no-extraneous-dependencies */
 import React from 'react'
 import { FaReact } from 'react-icons/fa'
 import { RiJavascriptLine } from 'react-icons/ri'
 import { TbBrandTypescript } from 'react-icons/tb'
+import { Navigation, Pagination, Scrollbar, A11y } from 'swiper/modules'
+import { Swiper, SwiperSlide } from 'swiper/react'
 import SkillCard from '../SkillCard'
 
 const Skills = () => (
   <div className=" mx-auto max-w-6xl pt-24 flex w-full  flex-wrap justify-between ">
-    <SkillCard
-      rotate="rotate-3"
-      title="React"
-      icons={<FaReact size={60} className="animate-spin-slow" />}
-    />
-    <SkillCard
-      rotate="-rotate-1"
-      title="Javascript"
-      icons={<RiJavascriptLine size={60} className="animate-pulse" />}
-    />
-    <SkillCard
-      rotate="rotate-3"
-      title="Typescript"
-      icons={<TbBrandTypescript size={60} className="animate-bounce" />}
-    />
+    <Swiper
+      className="h-[400px] f w-full"
+      // install Swiper modules
+      modules={[Navigation, Pagination, Scrollbar, A11y]}
+      spaceBetween={50}
+      slidesPerView={3}
+      navigation
+      pagination={{ clickable: true }}
+      scrollbar={{ draggable: true }}
+      onSwiper={(swiper) => console.log(swiper)}
+      onSlideChange={() => console.log('slide change')}
+    >
+      <SwiperSlide
+        style={{
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+        }}
+      >
+        <SkillCard
+          rotate="rotate-3"
+          title="React"
+          icons={<FaReact size={60} className="animate-spin-slow" />}
+        />
+      </SwiperSlide>
+
+      <SwiperSlide
+        style={{
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+        }}
+      >
+        <SkillCard
+          rotate="-rotate-1"
+          title="Javascript"
+          icons={<RiJavascriptLine size={60} className="animate-pulse" />}
+        />
+      </SwiperSlide>
+
+      <SwiperSlide
+        style={{
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+        }}
+      >
+        <SkillCard
+          rotate="rotate-3"
+          title="Typescript"
+          icons={<TbBrandTypescript size={60} className="animate-bounce" />}
+        />
+      </SwiperSlide>
+
+      <SwiperSlide
+        style={{
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+        }}
+      >
+        <SkillCard
+          rotate="rotate-3"
+          title="React"
+          icons={<FaReact size={60} className="animate-spin-slow" />}
+        />
+      </SwiperSlide>
+
+      <SwiperSlide
+        style={{
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+        }}
+      >
+        <SkillCard
+          rotate="-rotate-1"
+          title="Javascript"
+          icons={<RiJavascriptLine size={60} className="animate-pulse" />}
+        />
+      </SwiperSlide>
+
+      <SwiperSlide
+        style={{
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+        }}
+      >
+        <SkillCard
+          rotate="rotate-3"
+          title="Typescript"
+          icons={<TbBrandTypescript size={60} className="animate-bounce" />}
+        />
+      </SwiperSlide>
+
+      <SwiperSlide
+        style={{
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+        }}
+      >
+        <SkillCard
+          rotate="rotate-3"
+          title="React"
+          icons={<FaReact size={60} className="animate-spin-slow" />}
+        />
+      </SwiperSlide>
+
+      <SwiperSlide
+        style={{
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+        }}
+      >
+        <SkillCard
+          rotate="-rotate-1"
+          title="Javascript"
+          icons={<RiJavascriptLine size={60} className="animate-pulse" />}
+        />
+      </SwiperSlide>
+
+      <SwiperSlide
+        style={{
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+        }}
+      >
+        <SkillCard
+          rotate="rotate-3"
+          title="Typescript"
+          icons={<TbBrandTypescript size={60} className="animate-bounce" />}
+        />
+      </SwiperSlide>
+    </Swiper>
   </div>
 )
 
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,10 @@
+/* eslint-disable import/no-extraneous-dependencies */
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+import 'swiper/css'
+import 'swiper/css/navigation'
+import 'swiper/css/pagination'
+import 'swiper/css/scrollbar'
 
 export default function App({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
